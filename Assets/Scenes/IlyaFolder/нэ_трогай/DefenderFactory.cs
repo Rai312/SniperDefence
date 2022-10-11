f44bf3ee8384dc5b6c309c7c47b7cd04cb17d6db
Danil0990 <wpigyn@gmail.com>
2022-10-11 14:59:34 +0500

    newAsset

--- Assets/Scenes/IlyaFolder/нэ_трогай/DefenderFactory.cs
+++ Assets/Scenes/IlyaFolder/нэ_трогай/DefenderFactory.cs
@@ -1,18 +1,18 @@
 using System.Collections.Generic;
-using NativeSerializableDictionary;
+using BuildingBlocks.DataTypes;
 using UnityEngine;
 
 public class DefenderFactory : MonoBehaviour
 {
-  [SerializeField] private SerializableDictionary<int, DefenderSquad> _shootingMap = new Dictionary<int, DefenderSquad>();
-  [SerializeField] private SerializableDictionary<int, DefenderSquad> _fighterMap = new Dictionary<int, DefenderSquad>();
-  
+  [SerializeField] private InspectableDictionary<int, DefenderSquad> _shootingMap = new Dictionary<int, DefenderSquad>();
+  [SerializeField] private InspectableDictionary<int, DefenderSquad> _fighterMap = new Dictionary<int, DefenderSquad>();
+
   public DefenderSquad GetDefenderSquad(int key, DefenderType type)
   {
-    if (type == DefenderType.Fighter)
-      return _fighterMap[key].Value;
-    else if (type == DefenderType.Shooting)
-      return _shootingMap[key].Value;
+      if (type == DefenderType.Fighter && _fighterMap.TryGetValue(key, out DefenderSquad fighterSquad))
+      return fighterSquad;
+    else if (type == DefenderType.Shooting && _shootingMap.TryGetValue(key, out DefenderSquad shooterSquad))
+      return shooterSquad;
     else
       return null;
   }
